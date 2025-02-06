915c7df69def196b24f15e39a6802d4f4e4c101c
Krithika Jain <krithikajain@Krithikas-Air.eduroam.gmu.edu>
2025-02-06 09:45:40 -0500

    experimenting with svm data and predictions

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -13,6 +13,7 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 from sklearn import svm
+import numpy as np
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -36,20 +37,31 @@ The Code Below solves that issue, So use this one
 # features_train = features_train[:int(len(features_train)/100)]
 # labels_train = labels_train[:int(len(labels_train)/100)]
 
-C_values = [10.0, 100.0, 1000.0, 10000.0]
+# C_values = [10.0, 100.0, 1000.0, 10000.0]
 
-for C in C_values:
-    print(f"\nTesting SVM with RBF kernel and C = {C}")
-    svc = svm.SVC(kernel="rbf", C=C)
-    t0=time()
-    svc.fit(features_train, labels_train)
-    print("Training Time:", round(time()-t0, 3), "s")
+# for C in C_values:
+# print(f"\nTesting SVM with RBF kernel and C = {C}")
+svc = svm.SVC(kernel="rbf", C=1000.0)
+t0=time()
+svc.fit(features_train, labels_train)
+print("Training Time:", round(time()-t0, 3), "s")
 
-    t0=time()
-    svc.predict(features_test)
-    print("Predicting Time:", round(time()-t0, 3), "s")
+t0=time()
+predictions = svc.predict(features_test)
+print("Predicting Time:", round(time()-t0, 3), "s")
 
-    accuracy = svc.score(features_test, labels_test)
-    print("Accuracy", accuracy)
+# answer_10 = predictions[10]
+# answer_26 = predictions[26]
+# answer_50 = predictions[50]
+
+# print(f"Prediction for element 10: {answer_10}")
+# print(f"Prediction for element 26: {answer_26}")
+# print(f"Prediction for element 50: {answer_50}")
+
+num_chris = np.sum(predictions == 1)
+print(f"Number of test predictions labeled as 1, predicted to be Chris: {num_chris}" )
+
+accuracy = svc.score(features_test, labels_test)
+print("Accuracy", accuracy)
 
 #########################################################
